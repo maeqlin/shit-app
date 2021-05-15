6859758efbc524a19eeff9f652f4cf71385ae834
tufan <twofunlastwish@gmail.com>
2021-05-15 20:10:12 +0300

    install metamask

--- src/app/dialogs/install-metamask-dialog/install-metamask-dialog.component.ts
+++ src/app/dialogs/install-metamask-dialog/install-metamask-dialog.component.ts
@@ -1,26 +1,25 @@
-import { Inject } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { ConnectDialogComponent } from '../connect-dialog/connect-dialog.component';
+import { Inject } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { ConnectDialogComponent } from "../connect-dialog/connect-dialog.component";
 
 @Component({
-  selector: 'app-install-metamask-dialog',
-  templateUrl: './install-metamask-dialog.component.html',
-  styleUrls: ['./install-metamask-dialog.component.scss']
+  selector: "app-install-metamask-dialog",
+  templateUrl: "./install-metamask-dialog.component.html",
+  styleUrls: ["./install-metamask-dialog.component.scss"],
 })
 export class InstallMetamaskDialogComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data,
+    public dialogRef: MatDialogRef<ConnectDialogComponent>
+  ) {}
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data,
-  public dialogRef : MatDialogRef<ConnectDialogComponent>) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  installMetamask() {
+    window.open(
+      "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn",
+      "_blank"
+    );
   }
-
-
-
-  installMetamask(){
-    window.open('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn', '_blank');
-
-  }
-
 }
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -23,6 +23,31 @@ export class ToolbarComponent implements OnInit {
 
   connected = false;
 
+  chainid: string;
+
+  isMetamaskInstalled: boolean;
+
+  erc20Address = "0xf7c6d77e653af5fead1ce07ebd30f2910ce70ad8";
+
+  minABI = [
+    // balanceOf
+    {
+      constant: true,
+      inputs: [{ name: "_owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ name: "balance", type: "uint256" }],
+      type: "function",
+    },
+    // decimals
+    {
+      constant: true,
+      inputs: [],
+      name: "decimals",
+      outputs: [{ name: "", type: "uint8" }],
+      type: "function",
+    },
+  ];
+
   info: { currentAddress: string; balance: string };
 
   constructor(
@@ -48,16 +73,6 @@ export class ToolbarComponent implements OnInit {
     this.info = await this.contractService.getInfo();
     this.cdr.detectChanges();
     console.log(this.info);
-    // this.contractService.web3.eth.requestAccounts().then((e) => {
-    //   this.currentAddress = e[0];
-    //   this.contractService.web3.eth
-    //     .getBalance(this.currentAddress)
-    //     .then((e) => {
-    //       this.balance = e;
-    //     })
-    //     .catch((er) => {
-    //       console.log(er);
-    //     });
   }
 
   async checkMetaMaskConnection() {
@@ -65,20 +80,15 @@ export class ToolbarComponent implements OnInit {
 
     if (provider) {
       console.log("Ethereum successfully detected!");
-      
-      // provider.request({ method: 'eth_requestAccounts' }).then(e => {
 
-      //   console.log(e);
+      this.isMetamaskInstalled = true;
 
-      // });
-
-      provider.on('disconnect', (test) => {
+      provider.on("disconnect", (test) => {
         console.log("provider disconnected");
-          this.info = null;
-          localStorage.removeItem("info");
-          this.cdr.detectChanges();
+        this.info = null;
+        localStorage.removeItem("info");
+        this.cdr.detectChanges();
       });
-     
 
       (window.ethereum as any).on("accountsChanged", (accounts) => {
         // If user has locked/logout from MetaMask, this resets the accounts array to empty
@@ -92,9 +102,8 @@ export class ToolbarComponent implements OnInit {
           localStorage.removeItem("info");
           this.cdr.detectChanges();
           // logic to handle what happens once MetaMask is locked
-        }else {
-
-          
+        } else {
+          console.log("length missing");
         }
       });
 
@@ -108,8 +117,12 @@ export class ToolbarComponent implements OnInit {
       // show popup
       console.error("Please install MetaMask!");
 
-      this.installMetamask();
+      this.isMetamaskInstalled = false;
     }
+  }
+
+  getTokenBalance() {
+    console.log(this.minABI);
   }
 
   installMetamask() {
@@ -117,14 +130,21 @@ export class ToolbarComponent implements OnInit {
   }
 
   connectDialog() {
-    this.dialog.open(ConnectDialogComponent)
-    .afterClosed()
-    .subscribe(result=> {
-      if(result) {
-        console.log(result);
-        this.info = result.data;
-        this.cdr.detectChanges();
-      }
-    })
+    if (!this.isMetamaskInstalled) {
+      this.installMetamask();
+
+      return;
+    }
+
+    this.dialog
+      .open(ConnectDialogComponent)
+      .afterClosed()
+      .subscribe((result) => {
+        if (result) {
+          console.log(result);
+          this.info = result.data;
+          this.cdr.detectChanges();
+        }
+      });
   }
 }
